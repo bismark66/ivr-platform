8423807c542368e7bf97f3c5190dab269de51ad3
Bismark <agyarebismark6@gmail.com>
2024-09-21 01:55:35 +0000

    added a state to check path and display editor bar

--- src/app/page.js
+++ src/app/page.js
@@ -93,7 +93,29 @@ export default function Home() {
           </p>
         </a>
       </div> */}
-      <h2 style={{ textAlign: "center", color: "black" }}>IVR - Platform</h2>
+      <h2 style={{ textAlign: "center", color: "black" }}>
+        IVR - Platform lorem ipsumPellentesque laoreet erat vel nisi volutpat,
+        vitae eleifend lorem bibendum. Class aptent taciti sociosqu ad litora
+        torquent per conubia nostra, per inceptos himenaeos. Duis non lorem
+        blandit, sollicitudin lectus vitae, pharetra quam. Morbi ut facilisis
+        nunc. Quisque nisi lorem, sollicitudin eu tincidunt non, lobortis nec
+        odio. Duis in neque vitae purus finibus luctus. Donec consequat
+        imperdiet odio vitae tristique. Cras venenatis mauris quis rhoncus
+        efficitur. Quisque dignissim, ex ac vehicula pulvinar, dui elit varius
+        ex, sit amet fringilla magna libero in purus. Duis eu nulla nunc. Sed
+        vel diam varius, pellentesque nisi nec, mollis nisi. Praesent tincidunt,
+        diam eu ultricies pulvinar, justo lorem pellentesque enim, eu iaculis
+        tortor tortor vitae diam. In vitae lacus eros. Cras neque purus,
+        placerat quis interdum eu, imperdiet vel turpis. Ut tellus metus,
+        maximus quis lacus id, sollicitudin maximus felis. Donec finibus orci
+        vel augue iaculis porta. Pellentesque ut facilisis risus. Sed
+        condimentum sem id imperdiet ultricies. Etiam mi arcu, molestie et massa
+        vel, posuere iaculis metus. Vestibulum pulvinar augue et dignissim
+        luctus. Proin tristique lacus vel arcu finibus, eu placerat ex
+        fermentum. Phasellus non pellentesque nisi. Aliquam ante ex, accumsan
+        vitae aliquet ac, malesuada a ex. Etiam sagittis, libero eu vulputate
+        cursus, risus lectus hendrerit tellus, vel congue velit arcu id dolor.
+      </h2>
       <Button type="primary">Submit</Button>
     </main>
   );
