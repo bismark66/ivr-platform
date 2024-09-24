63cb44ccfabd4ad41919cc5eca7dab1758d280fd
Bismark <agyarebismark6@gmail.com>
2024-09-24 23:46:07 +0000

    added logic to handle two different modals

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,28 +1,89 @@
 /** @format */
 
 import React, { useState } from "react";
-import { Button, Modal } from "antd";
-function AppModal({ visible, onClose }) {
+import { Button, Modal, Row, Col, Form, Input } from "antd";
+import { ExclamationCircleFilled } from "@ant-design/icons";
+
+function AppModal({ visible, onClose, confirmSave }) {
   const handleClick = () => {
     prompt("reload");
   };
 
+  const onFinish = (values) => {
+    console.log("Received values of form: ", values);
+  };
+
   return (
     <>
-      <Modal
-        title={<p>Loading Modal</p>}
-        footer={
-          <Button type="primary" onClick={() => handleClick()}>
-            Reload
-          </Button>
-        }
-        open={visible}
-        onCancel={onClose}
-      >
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-      </Modal>
+      {confirmSave ? (
+        <Modal
+          title={
+            <>
+              <Row>
+                <Col span={20}>
+                  <h2>
+                    {" "}
+                    <ExclamationCircleFilled /> Do you want to save changes?
+                  </h2>
+                </Col>
+                <Col span={24} style={{ marginTop: "10px" }}>
+                  <Form
+                    onFinish={onFinish}
+                    style={{
+                      //   maxWidth: 600,
+                      width: "100%",
+                    }}
+                  >
+                    <Form.Item
+                      name="username"
+                      noStyle
+                      rules={[
+                        {
+                          required: true,
+                          message: "Username is required",
+                        },
+                      ]}
+                    >
+                      <Input
+                        style={{
+                          width: "100%",
+                        }}
+                        placeholder="Save As"
+                      />
+                    </Form.Item>
+                  </Form>
+                </Col>
+              </Row>
+            </>
+          }
+          footer={
+            <Button
+              type="primary"
+              htmlType="submit"
+              onClick={() => handleClick()}
+            >
+              Submit
+            </Button>
+          }
+          open={visible}
+          onCancel={onClose}
+        ></Modal>
+      ) : (
+        <Modal
+          title={<p>Loading Modal</p>}
+          footer={
+            <Button type="primary" onClick={() => handleClick()}>
+              Reload
+            </Button>
+          }
+          open={visible}
+          onCancel={onClose}
+        >
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+        </Modal>
+      )}
     </>
   );
 }
--- src/components/toolBar.js
+++ src/components/toolBar.js
@@ -1,8 +1,8 @@
 /** @format */
 "use client";
 
-import React from "react";
-import { Row, Col, Button } from "antd";
+import React, { useState } from "react";
+import { Row, Col, Button, Modal } from "antd";
 import MenuNode from "./steps/menu";
 import { useDispatch, useSelector } from "react-redux";
 import {
@@ -11,27 +11,58 @@ import {
   reset,
 } from "../utils/storeController";
 import Controller from "@/http";
+import AppModal from "./modal";
+import { ExclamationCircleFilled } from "@ant-design/icons";
 
 // const handleClick = () => {
 //     dispatch(toggleButtonClick()); // Dispatch action to change button click state
 // };
-function ToolBar({ addNode }) {
+const { confirm } = Modal;
+
+const showConfirm = () => {
+  confirm({
+    title: "Do you want to delete these items?",
+    icon: <ExclamationCircleFilled />,
+    content:
+      "When clicked the OK button, this dialog will be closed after 1 second.",
+    onOk() {
+      return new Promise((resolve, reject) => {
+        setTimeout(Math.random() > 0.5 ? resolve : reject, 1000);
+      }).catch(() => console.log("Oops errors!"));
+    },
+    onCancel() {
+      console.log("Cancel");
+    },
+  });
+};
+
+function ToolBar() {
   const dispatch = useDispatch();
   const { nodes, edges } = useSelector((state) => state.flow);
 
-  //   const handleClick = (nodeItem) => addNode(nodeItem);
+  const [open, setOpen] = useState(false);
+
+  const openModal = (state) => {
+    console.log("openModal", state);
+    setOpen(state);
+  };
+
+  const closeModal = () => {
+    setOpen(false);
+  };
+
   const handleClick = (nodeItem) => dispatch(updateMessage(nodeItem));
   const saveFlow = async () => {
     // const saveResponse = dispatch(saveFlowToFirestore()); // Trigger the save action
     // console.log("saveResponse", saveResponse);
-
+    openModal(true);
     if (nodes.length === 0 && edges.length === 0) {
       console.log("No data was found");
       return;
     }
 
-    const res = await Controller.saveFlowToFirestore(nodes, edges);
-    console.log("res", res);
+    // const res = await Controller.saveFlowToFirestore(nodes, edges);
+    // console.log("res", res);
   };
 
   const resetFlow = () => dispatch(reset());
@@ -47,6 +78,7 @@ function ToolBar({ addNode }) {
           height: "100%",
         }}
       >
+        <AppModal visible={open} onClose={closeModal} confirmSave={true} />
         <Row
           justify={"center"}
           align={"middle"}
@@ -100,6 +132,7 @@ function ToolBar({ addNode }) {
               style={{ width: "80%" }}
               type="primary"
               onClick={() => saveFlow()}
+              // onClick={showConfirm()}
             >
               Save
             </Button>
