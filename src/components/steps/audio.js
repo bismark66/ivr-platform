157ec16348cba19902ea469f92c4a7386dd3e705
Bismark <agyarebismark6@gmail.com>
2024-09-27 20:05:56 +0000

    remved unused code from audio.js

--- src/components/steps/audio.js
+++ src/components/steps/audio.js
@@ -2,10 +2,7 @@
 
 import React, { useCallback } from "react";
 import { Handle, Position } from "@xyflow/react";
-import { Dropdown, Space } from "antd";
-import { DownOutlined, SmileOutlined } from "@ant-design/icons";
 import "./menu.css";
-const handleStyle = { left: 10 };
 
 function AudioNode() {
   const onChange = useCallback((evt) => {
